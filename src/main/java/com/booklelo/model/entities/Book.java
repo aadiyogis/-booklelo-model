8a4bd051815f0f595687c9b17053e76953a7dcce
aditya <adiyogisiddha@gmail.com>
2020-08-12 17:40:29 +0530

    adding view for books

--- src/main/java/com/booklelo/model/entities/Book.java
+++ src/main/java/com/booklelo/model/entities/Book.java
@@ -28,10 +28,19 @@ public class Book {
     public Book() {
     }
 
-    public Book(String title, String isbn, Set<Author> authors) {
+    public Book(String title, String isbn, Set<Author> authors, Publisher publisher) {
         this.title = title;
         this.isbn = isbn;
         this.authors = authors;
+        this.publisher = publisher;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     public Long getId() {
--- src/main/java/com/booklelo/model/web/DataController.java
+++ src/main/java/com/booklelo/model/web/DataController.java
@@ -5,15 +5,16 @@ import com.booklelo.model.repositories.BookRepository;
 import com.booklelo.model.repositories.PublisherRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * aditya created on 10/08/20
  */
-@RestController
+@Controller
 @RequestMapping("/data")
 public class DataController {
 
@@ -27,14 +28,15 @@ public class DataController {
         this.publisherRepository = publisherRepository;
     }
 
-    @GetMapping("/books")
-    public ResponseEntity<?> getAllBooks() {
-        return new ResponseEntity<>(bookRepository.findAll(), HttpStatus.OK);
+    @RequestMapping(value = "/books", method = RequestMethod.GET)
+    public String getAllBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "books/list";
     }
 
 
     @GetMapping("/authors")
-    public ResponseEntity<?> getAllAuthors() {
+    public ResponseEntity<?> getAllAuthors(Model model) {
         return new ResponseEntity<>(authorRepository.findAll(), HttpStatus.OK);
     }
 
